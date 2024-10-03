9129d93014bb58f8f3b8fbe1f10e8852ed7d9b46
AmiraGamal1 <amiragamal098@gmail>
2024-10-03 13:22:19 +0000

    create files

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * class room
+ */
+export default class classRo0m {
+  /**
+   * Creates a new class room
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentSize) {
+    this._maxStudentSize = maxStudentSize;
+  }
+}
